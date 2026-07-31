162553f5d60e9a9be47e1b2a1fbb53b87b23d91f
tomwilsonsco <17555474+tomwilsonsco@users.noreply.github.com>
2026-07-31 13:16:15 +0100

    padding app

--- lex_eval/reports/streamlit_report.py
+++ lex_eval/reports/streamlit_report.py
@@ -723,7 +723,7 @@ def main() -> None:
     st.markdown(
         """
     <style>
-        .block-container { padding-top: 1.7rem; }
+        .block-container { padding-top: 1.8 rem; }
     </style>
 """,
         unsafe_allow_html=True,
